7274c096862e89ddc639a284271aa4c5f66428b6
Jose Oleriano <joserobertoadvir@gmail.com>
2024-01-08 08:45:40 -0300

    Ajuste do botao limpar

---
+++ app/src/main/java/oleriano/jose/alcoolgas/calculadoraActivity2.kt
@@ -0,0 +1,11 @@
+package oleriano.jose.alcoolgas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class calculadoraActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculadora)
+    }
+}
